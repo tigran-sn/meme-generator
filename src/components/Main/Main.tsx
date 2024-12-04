e2ba6845759c93aba3e977b170005744a510aba1
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-04 22:03:31 +0400

    Added handleChange method for the input fields

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,23 +1,48 @@
+/* eslint-disable no-debugger */
 import { useState } from "react";
 
+interface IMeme {
+  topText: string;
+  bottomText: string;
+  imageUrl: string;
+}
+
 export default function Main() {
-  const [meme, setMeme] = useState({
+  const [meme, setMeme] = useState<IMeme>({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     imageUrl: "https://imgflip.com/s/meme/One-Does-Not-Simply.jpg",
   });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme: IMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="One does not simply"
+            name="topText"
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Walk into Mordor"
+            name="bottomText"
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
